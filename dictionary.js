ad0514149d850c4340d0dd9c261a56371bb4ad36
incody0 <126239318+incody0@users.noreply.github.com>
2023-02-27 14:27:56 +0300

    dict

---
+++ dictionary.js
@@ -0,0 +1,57 @@
+const dictionary = {
+  "ceiling": "потолок",
+  "floor": "пол",
+  "danger": "опастность",
+"everybody": "каждый,все",
+ "somebody": "кто-нибудь", 
+ "carpet": "кавёр", 
+ "flat": "квартира", 
+"vacuum cleaner": "пылесос",
+"fall": "падать",  
+ "brave": "смелый", 
+ "steel": "сталь", 
+"brake": " ломаться",
+  "plate": "тарелка",
+  "teapot": "чайник",
+  "Mountain": "гора",
+  "Light": "свет",
+  "Bow": "дуга,лук",
+  "Chirch": "церковь",
+  "You had a card": "у тебя была карта",
+  "Just": "только что",
+  "If": "если"
+"Anyon": "любой"
+  "Witc": "с"
+  "Could": "могли"
+  "Allow": "разрешать"
+  "Believe": "Верить"
+
+"Only": "Только"
+
+"Adjust": "Настраивать"
+  "Any": "Любое"
+  "Must be done": "Должно быть сделано"
+
+"Hide": "Прятать"
+
+"Enough": "Достаточно"
+
+"Not enough": "Не достаточно"
+  "Abilities": "Способности"
+
+"Often": "Часто"
+
+."Speed": "Скорость"
+  "Everything": "Все"
+
+"Between": "Между"
+
+"Large": "Большой"
+
+"Area": "Область"
+
+"Small -> Medium -> Large -> Extra Large -> Huge": "Маленький - Средний - Большой - Очень большой - Огромный"
+
+"S -> M -> L -> XL"
+
+"In the middle": "В середине"
